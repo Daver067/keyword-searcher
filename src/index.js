fa6b837a9011b9d10b735d4ab76c089616408718
Dave <davediemert_88@hotmail.com>
2022-06-12 19:54:58 -0700

    working on the input module for keywords. Need to take time and put things down in pseudoCode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./style.scss";
+import { renderModule } from "./scripts/keywordModule";
 
-console.log("test");
+const display = document.getElementById("display");
+function openModuleAddKeywords() {
+  renderModule();
+}
+
+function moduleBtnEventListener(Btn) {
+  Btn.addEventListener("click", openModuleAddKeywords);
+}
+function createNewElement(type, addClass, innerHTML) {
+  const domElement = document.createElement(type);
+  domElement.classList.add(addClass);
+  domElement.innerHTML = innerHTML;
+  return domElement;
+}
+
+function addModuleBtn() {
+  const moduleBtn = createNewElement(
+    "button",
+    "moduleBtn",
+    "Click me to add keywords!"
+  );
+  moduleBtnEventListener(moduleBtn);
+  display.appendChild(moduleBtn);
+}
+
+export { createNewElement };
+
+addModuleBtn();
